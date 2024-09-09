ec9de3c80d8a1188a5b67ecd5d63c9d6d29ec1f0
Luke Gill <lukegill@macbook-pro.mynetworksettings.com>
2024-09-08 23:46:15 -0400

    adds navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :todo_lists do
     resources :tasks, only: [ :create, :edit, :show ]
-  end
+   end
   resources :tasks, only: [ :update, :index, :new ]
   root "todo_lists#index"
 end
