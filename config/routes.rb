bc2af2be39c9f162e6c3b596fa195f6fde444da8
Luke Gill <bagrad007@gmail.com>
2024-09-25 20:13:47 -0400

    can successfully share todos with other users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :todo_lists
   resources :tasks
-  resources :shared_todo_lists, only: [ :index, :create, :destroy, :show ]
+  resources :shared_todo_lists
   root "todo_lists#index"
 end
