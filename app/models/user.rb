52391bf637f2b54c1186dcfdecd3da567219b425
Luke Gill <bagrad007@gmail.com>
2025-08-04 00:26:28 -0400

    Destroy user dependencies

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :todo_lists
+  has_many :todo_lists, dependent: :destroy
   has_many :tasks, through: :todo_lists
 
 
-  has_many :shared_todo_lists
+  has_many :shared_todo_lists, dependent: :destroy
   has_many :shared_todos, through: :shared_todo_lists, source: :todo_list
   has_many :shared_tasks, through: :shared_todos, source: :tasks
 end
